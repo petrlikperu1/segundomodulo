f65eb954227b3778d8ebb70beb722fcf4a2027b6
Ivan <ivanpetrlikazabache@gmail.com>
2020-04-17 20:12:51 -0500

    Primera version
    Esta es la  primer version del modulo en Odoo

---
+++ __manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'segundomodulo',
+    'descripcion':'Modulo de ejemplo 2',
+    'version':'13.0.0.0.0',
+    'author':'Ivan Petrlik',
+    'data': [],
+    'depends':[],
+    'installable': True,
+    'application': True,
+    'auto_install':False,
+}
